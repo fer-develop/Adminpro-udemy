93c056ab0d2b30edb04097491c8046371aa31ea6
Alan <58962377+AlanFernandoProjects@users.noreply.github.com>
2019-12-29 13:17:31 -0700

    Agregacion de modulos y rutas hijas

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+
+// Modulos
+
+// Componentes
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+@NgModule({
+    declarations: [
+        NopagefoundComponent,
+        HeaderComponent,
+        SidebarComponent,
+        BreadcrumbsComponent
+    ],
+    exports: [
+        NopagefoundComponent,
+        HeaderComponent,
+        SidebarComponent,
+        BreadcrumbsComponent
+    ],
+    imports: [
+
+    ]
+})
+export class SharedModule { }
